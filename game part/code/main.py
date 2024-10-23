789995bac423089f6e10b913ed5e1b626c190ad8
hyeoksl <hyukkim9933@gmail.com>
2024-10-23 18:08:11 +0900

    뼈대만

--- game part/code/main.py
+++ game part/code/main.py
@@ -1,60 +1,207 @@
 from settings import *
+from game_data import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
-from level import Level
-from sprites import Sprite
-from entities import Player
+
+from sprites import Sprite, AnimatedSprite, MonsterPatchSprite, BorderSprite, CollidableSprite, TransitionSprite
+from entities import Player, Character
 from groups import AllSprites
+from dialog import DialogTree
+from support import *
 
 class Game:
-    def __init__(self):
-        pygame.init()
-        self.display_surface=pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption("Software Project")
-        self.clock=pygame.time.Clock()
-        
-        #groups
-        self.all_sprites=AllSprites()
-        
-        self.level=Level()
-        
-        self.import_assets()
-        #수정 요망 - 현재 위치가 어느 마을인지에 따라 다른 월드맵을 그려야 함.
-        self.setup(self.tmx_maps['world'], 'house')
-    
-    def import_assets(self):
-        #맵 가져오기 - 수정 요망
-        #마을 당 맵 하나 이렇게 설정할 텐데 마을이 여러 개면 다 가져와야 함
-        self.tmx_maps = {'world': load_pygame(join('..','data','maps','world.tmx'))}
-        
-    def setup(self, tmx_map, player_start_pos):
-        #월드맵 그리기 - 수정 요망
-        print(tmx_map)
-        for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles(): #tmx file에 따라 나뉜다
-            Sprite((x*TILE_SIZE, y*TILE_SIZE), surf, self.all_sprites)
-            
-        #플레이어 스타팅 포인트 - 수정 요망
-        #플레이어 스타팅 포인트를 랜덤하게 설정하는 다른 로직을 짤 것.
-        for obj in tmx_map.get_layer_by_name('Entities'):
-            if obj.name=='Player' and obj.properties['pos']==player_start_pos:
-                self.player=Player((obj.x, obj.y), self.all_sprites)
+	# general 
+	def __init__(self):
+		pygame.init()
+		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+		pygame.display.set_caption('Software Project')
+		self.clock = pygame.time.Clock()
 
-    def run(self):
-        while True:
-            # event loop
-            self.clock.tick(FPS)
-            dt=self.clock.tick()/1000
-            for event in pygame.event.get():
-                if event.type==pygame.QUIT:
-                    pygame.quit()
-                    exit()
-                    
-            # game logic
-            self.all_sprites.update(dt)
-            self.all_sprites.draw(self.player.rect.center)
-            self.level.run()
-            pygame.display.update()
-            
-if __name__=='__main__':
-    game=Game()
-    game.run()
+		# groups 
+		#data-map 폴더의 구조에 따라 수정 요망
+		self.all_sprites = AllSprites()
+		self.collision_sprites = pygame.sprite.Group()
+		self.character_sprites = pygame.sprite.Group()
+		self.transition_sprites = pygame.sprite.Group()
+		self.monster_sprites = pygame.sprite.Group()
+
+		# transition / tint
+		self.transition_target = None
+		self.tint_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+		self.tint_mode = 'untint'
+		self.tint_progress = 0
+		self.tint_direction = -1
+		self.tint_speed = 600
+
+		self.import_assets()
+		self.setup(self.tmx_maps['world'], 'house')
+		self.audio['overworld'].play(-1)
+
+		# overlays 
+		self.dialog_tree = None
+		self.index_open = False
+
+	def import_assets(self):
+		#수정 요망 - 우리는 마을을 두 개 만드는 거니까 마을마다 저게 하나씩 있어야 함.
+		#즉 현재 플레이어가 있는 마을이 뭔지를 파악하고 그 마을의 그래픽을 로딩하는 작업이 필요함
+		self.tmx_maps = tmx_importer('..', 'data', 'maps')
+
+		self.overworld_frames = {
+			'water': import_folder('..', 'graphics', 'tilesets', 'water'),
+			'coast': coast_importer(24, 12, '..', 'graphics', 'tilesets', 'coast'),
+			'characters': all_character_import('..', 'graphics', 'characters')
+		}
+		self.fonts = {
+			'dialog': pygame.font.Font(join('..', 'graphics', 'fonts', 'PixeloidSans.ttf'), 30),
+			'regular': pygame.font.Font(join('..', 'graphics', 'fonts', 'PixeloidSans.ttf'), 18),
+			'small': pygame.font.Font(join('..', 'graphics', 'fonts', 'PixeloidSans.ttf'), 14),
+			'bold': pygame.font.Font(join('..', 'graphics', 'fonts', 'dogicapixelbold.otf'), 20),
+		}
+		self.bg_frames = import_folder_dict('..', 'graphics', 'backgrounds')
+		self.start_animation_frames = import_folder('..', 'graphics', 'other', 'star animation')
+	
+		self.audio = audio_importer('..', 'audio')
+
+	def setup(self, tmx_map, player_start_pos):
+		#맵 파일 구조에 따라 아주 많이 달라지는 부분
+  
+		# clear the map
+		for group in (self.all_sprites, self.collision_sprites, self.transition_sprites, self.character_sprites):
+			group.empty()
+
+		# terrain
+		for layer in ['Terrain', 'Terrain Top']:
+			for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+				Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites, WORLD_LAYERS['bg'])
+
+		# water 
+		for obj in tmx_map.get_layer_by_name('Water'):
+			for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
+				for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
+					AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites, WORLD_LAYERS['water'])
+
+		# coast
+		for obj in tmx_map.get_layer_by_name('Coast'):
+			terrain = obj.properties['terrain']
+			side = obj.properties['side']
+			AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites, WORLD_LAYERS['bg'])
+		
+		# objects 
+		for obj in tmx_map.get_layer_by_name('Objects'):
+			if obj.name == 'top':
+				Sprite((obj.x, obj.y), obj.image, self.all_sprites, WORLD_LAYERS['top'])
+			else:
+				CollidableSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
+
+		# transition objects
+		for obj in tmx_map.get_layer_by_name('Transition'):
+			TransitionSprite((obj.x, obj.y), (obj.width, obj.height), (obj.properties['target'], obj.properties['pos']), self.transition_sprites)
+
+		# collision objects 
+		for obj in tmx_map.get_layer_by_name('Collisions'):
+			BorderSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+		# grass patches 
+		for obj in tmx_map.get_layer_by_name('Monsters'):
+			MonsterPatchSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.monster_sprites), obj.properties['biome'], obj.properties['monsters'], obj.properties['level'])
+
+		# entities 
+		for obj in tmx_map.get_layer_by_name('Entities'):
+			if obj.name == 'Player':
+				if obj.properties['pos'] == player_start_pos:
+					self.player = Player(
+						pos = (obj.x, obj.y), 
+						frames = self.overworld_frames['characters']['player'], 
+						groups = self.all_sprites,
+						facing_direction = obj.properties['direction'], 
+						collision_sprites = self.collision_sprites)
+			else:
+				Character(
+					pos = (obj.x, obj.y), 
+					frames = self.overworld_frames['characters'][obj.properties['graphic']], 
+					groups = (self.all_sprites, self.collision_sprites, self.character_sprites),
+					facing_direction = obj.properties['direction'],
+					character_data = TRAINER_DATA[obj.properties['character_id']],
+					player = self.player,
+					create_dialog = self.create_dialog,
+					collision_sprites = self.collision_sprites,
+					radius = obj.properties['radius'],
+					nurse = obj.properties['character_id'] == 'Nurse',
+					notice_sound = self.audio['notice'])
+
+	# dialog system
+    #이건 따로 빼서 만들자. GPT 부분 있어야 하니까
+	def input(self):
+		if not self.dialog_tree:
+			keys = pygame.key.get_just_pressed()
+			if keys[pygame.K_SPACE]:
+				for character in self.character_sprites:
+					if check_connections(100, self.player, character):
+						self.player.block()
+						character.change_facing_direction(self.player.rect.center)
+						self.create_dialog(character)
+						character.can_rotate = False
+
+			if keys[pygame.K_RETURN]:
+				self.index_open = not self.index_open
+				self.player.blocked = not self.player.blocked
+
+	def create_dialog(self, character):
+		if not self.dialog_tree:
+			self.dialog_tree = DialogTree(character, self.player, self.all_sprites, self.fonts['dialog'], self.end_dialog)
+
+	def end_dialog(self, character):
+		self.dialog_tree = None
+		self.player.unblock()
+
+	# transition system
+	def transition_check(self):
+		sprites = [sprite for sprite in self.transition_sprites if sprite.rect.colliderect(self.player.hitbox)]
+		if sprites:
+			self.player.block()
+			self.transition_target = sprites[0].target
+			self.tint_mode = 'tint'
+
+	def tint_screen(self, dt):
+		if self.tint_mode == 'untint':
+			self.tint_progress -= self.tint_speed * dt
+
+		if self.tint_mode == 'tint':
+			self.tint_progress += self.tint_speed * dt
+			if self.tint_progress >= 255:
+				self.setup(self.tmx_maps[self.transition_target[0]], self.transition_target[1])
+				self.tint_mode = 'untint'
+				self.transition_target = None
+
+		self.tint_progress = max(0, min(self.tint_progress, 255))
+		self.tint_surf.set_alpha(self.tint_progress)
+		self.display_surface.blit(self.tint_surf, (0,0))
+  
+	def run(self):
+		while True:
+			dt = self.clock.tick(FPS) / 1000
+			self.display_surface.fill('black')
+
+			# event loop 
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					exit()
+
+			# update 
+			self.input()
+			self.transition_check()
+			self.all_sprites.update(dt)
+			
+			# drawing
+			self.all_sprites.draw(self.player)
+			
+			# overlays 
+			if self.dialog_tree: self.dialog_tree.update()
+
+			self.tint_screen(dt)
+			pygame.display.update()
+
+if __name__ == '__main__':
+	game = Game()
+	game.run()
